aef7e2c15b3813e9788887374ae4c3735d7a6945
Ellis Taylor <ellis.taylor@live.com>
2024-03-02 16:48:42 +0000

    set up initial app to test and connect css

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn spamazon.wsgi
---
+++ env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://gifnfhme:hEv0SQ_b37aW3n1Mt6e74f3lkGel56E6@flora.db.elephantsql.com/gifnfhme")
+os.environ.setdefault("SECRET_KEY", "RkRs2Uj1Z^>>wjn")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://371429349574452:ce9Y-0-wav4lPPOpneOG7TodeEA@dpwjsiyr3")
+
+
+
+
+
